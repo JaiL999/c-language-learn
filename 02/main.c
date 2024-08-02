52fb340bea69c00ef6897d9e4c1082d47a82424c
JaiL999 <18928314820@163.com>
2024-08-02 22:17:57 +0800

    Use the Google coding standard

--- 02/main.c
+++ 02/main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
-int a=9,b=7;
-a=a^b;
-b=b^a;
-a=a^b;
-printf("%d,%d\n",a,b);
-return 0;
+    int a=9,b=7;
+    a=a^b;
+    b=b^a;
+    a=a^b;
+    printf("%d,%d\n",a,b);
+    return 0;
 }
